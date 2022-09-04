842b3b8a480f0090459f5dbdd0ca064bac56972c
vincevel <83172424+vincevel@users.noreply.github.com>
2022-09-04 20:44:20 +0800

    Add files via upload

---
+++ dist/js/ugbuttonloader_1.js
@@ -0,0 +1,52 @@
+
+
+  Vue.component('ugbuttonloader', {
+    props: ['type','lang'],
+    data: function () {
+      return {
+        ltype: this.type,
+        llang: this.lang,
+        lhref: "",
+        bot: false
+      }
+    },
+    mounted: function () {
+      var postData = new FormData();
+      postData.append('type', this.ltype);
+      postData.append('lang', this.llang);
+ 
+      axios.post('https://www.vmvcore.com/api/ugbuttonloader', postData) 
+      .then(results => {
+          this.lhref = results.data[0];      
+      })
+
+    },
+    methods: {
+      getPage(e){
+      }      
+    },
+    template: `
+    <div id="ugbutton_container" class="">
+      <a :href="lhref">
+      <button class="learn-more">
+        <span class="circle" aria-hidden="true">
+          <span class="icon arrow"></span>
+        </span>
+        <span class="button-text">User Guide </span> <span class="button-icon"><i class="material-icons">import_contacts</i></span>
+      </button>
+      </a>
+    </div>
+    `
+  })
+   
+  var ugbuttonloaderapp = new Vue({
+    el: '#ugbuttonloaderapp',
+    data: {
+      state: [],
+      message: ''
+    },
+    methods: {
+          
+    },
+  })
+ 
